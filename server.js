cb797dcfc8cd8d4e9659708b199143d82525199a
gautamgavande <gautamgavande2436@gmail.com>
2024-08-29 16:54:31 +0530

    new api created

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app=express()
 // const port=process.env.PORT || 3000
 
 app.get("/api/get",(req,res)=>{
-    res.send({message:"node js aws deployment"})
+    res.send({message:"node js aws deployment",deploy:"your project is deploy"})
 })
 
 app.listen(8000,()=>{
