cd8cf59b7aee47c3be09afceeef00ac38e6ebaae
gautamgavande <gautamgavande2436@gmail.com>
2024-08-29 17:09:43 +0530

    nnn

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app=express()
 // const port=process.env.PORT || 3000
 
 app.get("/api/get",(req,res)=>{
-    res.send({message:"node js aws depl",deploy:"your project is deploy"})
+    res.send({message:"node js aws",deploy:"your project is deploy",person:"gautam"})
 })
 
 app.listen(8000,()=>{
